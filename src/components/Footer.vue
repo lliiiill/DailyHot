18d6c1aaab58e5d0a8b3636945bbe30ba8aa3f74
lliiiill <130821086+lliiiill@users.noreply.github.com>
2023-05-09 18:45:45 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,34 +1,9 @@
 <template>
   <footer>
-    <div class="copyright">
-      <n-text class="description" v-html="packageJson.description" />
-      <n-text
-        class="author"
-        :depth="3"
-        v-html="packageJson.author"
-        @click="jumpLink(packageJson.github)"
-      />
-    </div>
-    <n-text
-      v-if="icp"
-      :depth="3"
-      class="icp"
-      v-html="icp"
-      @click="jumpLink('https://beian.miit.gov.cn/')"
-    />
+
   </footer>
 </template>
 
-<script setup>
-import packageJson from "@/../package.json";
-
-const icp = ref(import.meta.env.VITE_ICP ? import.meta.env.VITE_ICP : null);
-
-// 链接跳转
-const jumpLink = (url) => {
-  window.open(url);
-};
-</script>
 
 <style lang="scss" scoped>
 footer {
